77dc00c5d12f79971c42227c4b20648ab330b8e6
Will Brennan <willfbren@gmail.com>
2020-02-28 21:07:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -59,10 +59,11 @@ def apply_clearance(cart)
   counter = 0 
   while counter < cart.length 
     if cart[counter][:clearance]
-      cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.20))
+      cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.20)).round(2)
     end
     counter += 1
   end
+  cart
 end
 
 def checkout(cart, coupons)
